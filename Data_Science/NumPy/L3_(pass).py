9e9804dcc742c2dd54b239a611551088f9931dff
Marwan Fahad <mamemo1270@gmail.com>
2025-07-14 22:04:25 +0300

    operation on array

---
+++ Data_Science/NumPy/L3_(pass).py
@@ -0,0 +1,35 @@
+import numpy as np
+
+
+#========================== coping
+# be careful when coping
+
+a = np.array([1,2,3])
+b = a
+b[0] = 99
+print(b)
+print(a) # changed because b was pointing to a   (pointer)
+
+a = np.array([1,2,3])
+b = a.copy() # this is real coping not pointing
+b[0] = 99
+print(b)
+print(a)
+print("copy finish---")
+print()
+#-----
+
+#========================== math
+
+a = np.array([1,2,3])
+a = a**2
+print(a)
+a = a - (a - 1)
+print(a)
+b = np.ones(a.shape,int)
+a +=b
+print(a)
+# and so on ....
+print("math finish---")
+print()
+#-----
