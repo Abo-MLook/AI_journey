6ec3cd910bd0f5c060d5aeadd210d2632e0452f5
Marwan Fahad <mamemo1270@gmail.com>
2025-07-21 13:16:00 +0300

    Create urls.py

---
+++ Data_Science/Regular Expressions(re)/urls.py
@@ -0,0 +1,16 @@
+import re
+
+urls = '''
+https://www.google.com
+http://coreyms.com
+https://youtube.com
+https://www.nasa.gov
+'''
+
+pattern = re.compile(r'')
+
+
+matches = pattern.finditer(urls)
+
+for match in matches:
+     print(match.group(3))
