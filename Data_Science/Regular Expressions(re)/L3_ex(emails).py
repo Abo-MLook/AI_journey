d72a1098337c7e4b2fae1b6137a3b152340a63ba
Marwan Fahad <mamemo1270@gmail.com>
2025-07-21 13:10:08 +0300

    Update L3_ex(emails).py

--- Data_Science/Regular Expressions(re)/L3_ex(emails).py
+++ Data_Science/Regular Expressions(re)/L3_ex(emails).py
@@ -5,15 +5,45 @@ CoreyMSchafer@gmail.com
 corey.schafer@university.edu
 corey-321-schafer@my-work.net
 '''
-
-
-# ======================================================================= matching from file
-pattern = re.compile(r'')
-
+# ======================================================================= matching first email
+pattern = re.compile(r'[a-zA-Z]+@[a-z]+\.com')
 matches = pattern.finditer(emails)
 
 for match in matches:
     print(match)
+print()
+#-------------------------------------------------------------exit
+# ======================================================================= matching first email
+pattern = re.compile(r'[a-z]+.[a-z]+@[a-z]+\.edu')
+matches = pattern.finditer(emails)
 
+for match in matches:
+    print(match)
+print()
+#-------------------------------------------------------------exit
+# ======================================================================= matching first email
+pattern = re.compile(r'[a-z]+-\d+-[a-z]+@[a-z]+-[a-z]+\.net')
+matches = pattern.finditer(emails)
+
+for match in matches:
+    print(match)
+print()
+#-------------------------------------------------------------exit
+
+# ======================================================================= matching first email
+pattern = re.compile(r'[a-zA-Z.]+@[a-z]+\.(com|edu)')
+matches = pattern.finditer(emails)
+
+for match in matches:
+    print(match)
+print()
+#-------------------------------------------------------------exit
+
+# ======================================================================= matching first email
+pattern = re.compile(r'[a-zA-Z\d.\-]+@[a-z\-]+\.(com|edu|net)')
+matches = pattern.finditer(emails)
+
+for match in matches:
+    print(match)
 print()
 #-------------------------------------------------------------exit
