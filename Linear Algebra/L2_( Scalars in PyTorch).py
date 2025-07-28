febe5a38ff2343c729c77a697b968df82cdbaaff
Marwan Fahad <mamemo1270@gmail.com>
2025-07-28 14:49:46 +0300

    Update L2_( Scalars in PyTorch).py

--- Linear Algebra/L2_( Scalars in PyTorch).py
+++ Linear Algebra/L2_( Scalars in PyTorch).py
@@ -1,1 +1,9 @@
 import torch
+# Scalars in PyTorch
+# PyTorch tensors are designed to be pythonic, i.e., to feel and behave like NumPy arrays
+# The advantage of PyTorch tensors relative to NumPy arrays is that they easily be used for operations on GPU
+# more on : https://docs.pytorch.org/docs/stable/tensors.html
+
+x = 25
+x_pt = torch.tensor(25) # type specification optional, e.g.: dtype=torch.float16
+print(x_pt.shape) # because it is scalser there is no dimtinosions
