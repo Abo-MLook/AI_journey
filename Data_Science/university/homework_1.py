6a526484019932010760bd0d479f3a94a7bcc3b1
Marwan Fahad <mamemo1270@gmail.com>
2025-07-29 11:05:44 +0300

    Update homework_1.py

--- Data_Science/university/homework_1.py
+++ Data_Science/university/homework_1.py
@@ -4,13 +4,13 @@ from tabulate import tabulate
 
 data = []
 for i in range(5):
-    sensor_id = f"S{random.randint(100,999)}"
+    sensor_id = f"S{random.randint(100,999)}" # id for sensor
     temperature = round(random.uniform(15.0, 35.0), 2)
     humidity = round(random.uniform(30.0, 90.0), 2)
     status = "High" if temperature > 30 else "Normal"
     timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
     data.append([sensor_id, temperature, humidity, status, timestamp])
 
-# Pretty table
+# table for displaying
 headers = ["Sensor ID", "Temperature (°C)", "Humidity (%)", "Status", "Timestamp"]
 print(tabulate(data, headers=headers, tablefmt="fancy_grid"))
