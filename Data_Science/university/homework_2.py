2a2b7db8b4d92e57477caf8cad5e8100b6be3774
Marwan Fahad <mamemo1270@gmail.com>
2025-08-02 21:20:17 +0300

    ش

--- Data_Science/university/homework_2.py
+++ Data_Science/university/homework_2.py
@@ -2,29 +2,28 @@ from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 import numpy as np
 
-# IoT sensor data
+# Data for clustering
 temperature = [25, 30, 35, 28, 32]
 humidity = [60, 55, 70, 65, 50]
 
-# Combine temperature and humidity into a 2D array
+# Combine data into a 2D array
 data = np.array(list(zip(temperature, humidity)))
 
-# Apply K-Means Clustering (2 clusters)
+# Apply K-Means Clustering
 kmeans = KMeans(n_clusters=2, random_state=42)
 kmeans.fit(data)
 
-# Get cluster labels for each data point
+# Get and print results
 labels = kmeans.labels_
-# Get cluster centers
 centroids = kmeans.cluster_centers_
 
-# Print results
 print("Cluster Labels for each data point:")
 print(labels)
+
 print("\nCluster Centers (Temperature, Humidity):")
 print(centroids)
 
-# Plot the clustered data
+# Plot the clusters and centroids
 plt.scatter(data[:, 0], data[:, 1], c=labels, cmap='viridis', label="Data Points")
 plt.scatter(centroids[:, 0], centroids[:, 1], color='red', marker='x', s=200, label="Centroids")
 plt.xlabel("Temperature (°C)")
