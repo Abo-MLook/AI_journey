e20c830f6a2662de346c4895791f111755f33d5f
Marwan Fahad <mamemo1270@gmail.com>
2025-08-22 18:56:37 +0300

    Update L(orthogonal ex).py

--- Linear Algebra/L(orthogonal ex).py
+++ Linear Algebra/L(orthogonal ex).py
@@ -16,3 +16,23 @@ print()
 print("c1 unit form : ",np.linalg.norm(c1))
 print("c2 unit form : ",np.linalg.norm(c2))
 print("c3 unit form : ",np.linalg.norm(c3))
+print()
+print()
+
+
+
+K = np.array([[2/3,-2/3,1/3],[1/3,2/3,2/3],[2/3,1/3,-2/3]])
+print(K)
+
+c1 = K[:,0]
+c2 = K[:,1]
+c3 = K[:,2]
+
+print()
+print("c1 dot c2 =", abs(round(np.dot(c1,c2), 12)))
+print("c1 dot c3 =", round(np.dot(c1,c3), 12))
+print("c2 dot c3 =", abs(round(np.dot(c2,c3), 12)))
+print()
+print("c1 norm =", round(np.linalg.norm(c1), 12))
+print("c2 norm =", round(np.linalg.norm(c2), 12))
+print("c3 norm =", round(np.linalg.norm(c3), 12))
