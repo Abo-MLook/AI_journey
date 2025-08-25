5d6b80482a7cebe4fbd9acd7e86b0892edc2482e
Marwan Fahad <mamemo1270@gmail.com>
2025-08-26 01:23:09 +0300

    Update more.py

--- Linear_Algebra/Determinant_&_Eigenvalues/more.py
+++ Linear_Algebra/Determinant_&_Eigenvalues/more.py
@@ -15,8 +15,20 @@ plt.xlim(-1, 3)
 _ = plt.ylim(-1, 3)
 plt.show()
 
+B = np.array([[1, 0], [0, 1]])
 N = np.array([[-4, 1], [-8, 2]])
 detN = np.linalg.det(N)
-
+print(f"det = {detN}")
 NB = np.dot(N, B)
 print(NB)
+
+
+plot_vectors([vectorfy(B, 0), vectorfy(B, 1), vectorfy(NB, 0), vectorfy(NB, 1)],
+            ['lightblue', 'lightgreen', 'blue', 'green'])
+plt.xlim(-6, 6)
+_ = plt.ylim(-9, 3)
+plt.show()
+lambdas, V = np.linalg.eig(N)
+print(f"EigenValues : \n{lambdas}")
+# Aha! If any one of a matrix's eigenvalues is zero, then the product of the eigenvalues must
+# be zero and the determinant must also be zero.
