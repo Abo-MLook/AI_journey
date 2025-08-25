68c42dd0941b1bb4b8e46d9db5bf763b478e377b
Marwan Fahad <mamemo1270@gmail.com>
2025-08-26 00:47:58 +0300

    Update excersice.py

--- Linear_Algebra/Matrix_Determinants/excersice.py
+++ Linear_Algebra/Matrix_Determinants/excersice.py
@@ -2,7 +2,8 @@ import numpy as np
 
 def inversable(mat):
     det = np.linalg.det(mat)
-    if det > 0 :
+    if det != 0 :
+        print(f"det = {det}")
         return "has an inverse\n"
     else:
         return "it is singular\n"
@@ -14,6 +15,8 @@ mat1 = np.array([[25,2],[3,4]])
 mat2 = np.array([[-2,0],[0,-2]])
 mat3 = np.array([[2,1,-3],[4,-5,2],[0,-1,3]])
 
-inversable(mat1)
-inversable(mat2)
-inversable(mat3)
+print(inversable(mat1))
+print(inversable(mat2))
+print(inversable(mat3))
+
+
