2ae7fed921331d8ebdb0d54812fc8c0f07831cbc
Marwan Fahad <mamemo1270@gmail.com>
2025-08-25 23:58:34 +0300

    Update intro.py

--- Linear_Algebra/Matrix_Determinants/intro.py
+++ Linear_Algebra/Matrix_Determinants/intro.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 
 X = np.array([[4, 2], [-5, -3]])
 
@@ -8,3 +9,12 @@ print(f"determinant = {detX}\n")
 N = np.array([[-4, 1], [-8, 2]])
 detN = np.linalg.det(N)
 print(f"determinant = {detN}\n")
+
+
+# Uncommenting the following line results in a "singular matrix" error
+# Ninv = np.linalg.inv(N)
+
+# in pyTorch
+N = torch.tensor([[-4, 1], [-8, 2.]]) # must use float not int
+detNt = torch.det(N)
+print(f"determinant = {detNt}")
