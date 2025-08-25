b6c5175ba038b77dfa2b03bb8a5cdbc07f4220b4
Marwan Fahad <mamemo1270@gmail.com>
2025-08-25 22:25:22 +0300

    Update intro.py

--- Linear_Algebra/L(Eigenvectors_Eigenvalues)/intro.py
+++ Linear_Algebra/L(Eigenvectors_Eigenvalues)/intro.py
@@ -33,3 +33,29 @@ plot_vectors([Av, v1], ['blue', 'lightblue'])
 plt.xlim(-1, 2)
 _ = plt.ylim(-1, 2)
 plt.show()
+
+
+# And again for the second eigenvector of A:
+
+
+v2 = V[:,1]
+lambdas2 = lambdas[1]
+print()
+print()
+
+Av2 = np.dot(A,v2) # Av
+print(Av2)
+
+print()
+
+Lv = lambdas2 * v2 # λv
+print(Lv)
+
+print(f"Av is == λv ? : {Av2==Lv}")
+
+
+plot_vectors([Av, v1, Av2, v2],
+            ['blue', 'lightblue', 'green', 'lightgreen'])
+plt.xlim(-1, 4)
+_ = plt.ylim(-3, 2)
+plt.show()
