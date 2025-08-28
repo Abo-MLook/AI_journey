f0c14cfb366ca7156cfc6f0724a88e4084cc4d6b
Marwan Fahad <mamemo1270@gmail.com>
2025-08-28 22:13:17 +0300

    update

--- Linear_Algebra/linear_system/intro.py
+++ Linear_Algebra/linear_system/intro.py
@@ -38,3 +38,34 @@ print(f"Determinant of matrix A: {d:.2f}")
 print()
 A_system = np.hstack((A, b.reshape((2, 1))))
 print(A_system)
+plot_lines(A_system,(-10,10))
+
+
+
+#3 - System of Linear Equations with No Solutions
+
+
+A_2 = np.array([
+        [-1, 3],
+        [3, -9]
+    ], dtype=np.dtype(float))
+
+b_2 = np.array([7, 1], dtype=np.dtype(float))
+
+d_2 = np.linalg.det(A_2)
+
+print(f"Determinant of matrix A_2: {d_2:.2f}")
+try:
+    x_2 = np.linalg.solve(A_2, b_2)
+except np.linalg.LinAlgError as err:
+    print(err)
+
+A_2_system = np.hstack((A_2, b_2.reshape((2, 1))))
+print(A_2_system)
+plot_lines(A_2_system)
+
+# 4 - System of Linear Equations with an Infinite Number of Solutions
+b_3 = np.array([7, -21], dtype=np.dtype(float))
+A_3_system = np.hstack((A_2, b_3.reshape((2, 1))))
+print(A_3_system)
+plot_lines(A_3_system)
--- Linear_Algebra/ploting/intro.py
+++ Linear_Algebra/ploting/intro.py
@@ -1,40 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-A = np.array([
-        [-1, 3],
-        [3, 2]
-    ], dtype=np.dtype(float))
-
-b = np.array([7, 1], dtype=np.dtype(float))
-
-print("Matrix A:")
-print(A)
-print("\nArray b:")
-print(b)
-
-print(f"Shape of A: {A.shape}")
-print(f"Shape of b: {b.shape}")
-
-# print(f"Shape of A: {np.shape(A)}")
-# print(f"Shape of A: {np.shape(b)}")
-
-
-x = np.linalg.solve(A, b)
-
-print(f"Solution: {x}")
-
-
-
-d = np.linalg.det(A)
-
-print(f"Determinant of matrix A: {d:.2f}")
-
-
-#2 - Visualizing 2x2 Systems as Plotlines
-print()
-A_system = np.hstack((A, b.reshape((2, 1))))
-print(A_system)
 
 
 def plot_lines(A_system, axis_range=(-10, 10)):
@@ -77,4 +43,3 @@ def plot_lines(A_system, axis_range=(-10, 10)):
     plt.ylabel("x2", fontsize=12)
     plt.title("System of Linear Equations", fontsize=14)
     plt.show()
-plot_lines(A_system,(-10,10))
