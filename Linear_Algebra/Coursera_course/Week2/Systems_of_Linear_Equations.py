c0ed211f4d7f98fa61cb26462d1e62af05c352bd
Marwan Fahad <mamemo1270@gmail.com>
2025-09-05 19:21:06 +0300

    Update Systems_of_Linear_Equations.py

--- Linear_Algebra/Coursera_course/Week2/Systems_of_Linear_Equations.py
+++ Linear_Algebra/Coursera_course/Week2/Systems_of_Linear_Equations.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+# 1.2 - Solving Systems of Linear Equations using Matrices :
 
 A = np.array([
         [4, -3, 1],
@@ -11,5 +12,26 @@ b = np.array([-10, 0, 17], dtype=np.dtype(float))
 
 print("Matrix A:")
 print(A)
+print()
 print("\nArray b:")
 print(b)
+print()
+
+
+print(f"Shape of A: {np.shape(A)}")
+print(f"Shape of b: {np.shape(b)}")
+print()
+
+
+
+x = np.linalg.solve(A, b)
+
+print(f"Solution: {x}")
+print()
+
+#========================================================
+
+# 1.3 - Evaluating the Determinant of a Matrix
+d = np.linalg.det(A)
+
+print(f"Determinant of matrix A: {d:.2f}")
