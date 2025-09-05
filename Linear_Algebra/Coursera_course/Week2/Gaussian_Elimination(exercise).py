b3d9b22e5ff6c669faff426506eee5612332b29a
Marwan Fahad <mamemo1270@gmail.com>
2025-09-05 19:45:03 +0300

    Create Gaussian_Elimination(exercise).py

---
+++ Linear_Algebra/Coursera_course/Week2/Gaussian_Elimination(exercise).py
@@ -0,0 +1,38 @@
+import numpy as np
+
+
+def swap_rows(M, row_index_1, row_index_2):
+    """
+    Swap rows in the given matrix.
+
+    Parameters:
+    - matrix (numpy.array): The input matrix to perform row swaps on.
+    - row_index_1 (int): Index of the first row to be swapped.
+    - row_index_2 (int): Index of the second row to be swapped.
+    """
+
+    # Copy matrix M so the changes do not affect the original matrix.
+    M = M.copy()
+    # Swap indexes
+    M[[row_index_1, row_index_2]] = M[[row_index_2, row_index_1]]
+    return M
+
+
+
+A_2= np.array([
+        [1, 1, 1],
+        [0, 1, -3],
+        [2, 1, 5]
+    ], dtype=np.dtype(float))
+
+b_2 = np.array([2, 1, 0], dtype=np.dtype(float))
+print(A_2.shape)
+print(b_2.shape)
+b_2 = b_2.reshape(-1, 1)
+
+A =np.hstack((A_2,b_2))
+print(A)
+print()
+
+A = swap_rows(A,0,2)
+print(A)
