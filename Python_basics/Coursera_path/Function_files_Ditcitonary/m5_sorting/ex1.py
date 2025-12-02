add51cb61896718d91e383f5ec33949824400a1e
Marwan Fahad <mamemo1270@gmail.com>
2025-12-02 21:40:41 +0300

    Update ex1.py

--- Python_basics/Coursera_path/Function_files_Ditcitonary/m5_sorting/ex1.py
+++ Python_basics/Coursera_path/Function_files_Ditcitonary/m5_sorting/ex1.py
@@ -1,3 +1,18 @@
 lst = [3, 5, 1, 6, 7, 2, 9, -2, 5]
 lst_sorted = sorted(lst,reverse=True)
 print(lst_sorted)
+print()
+
+L1 = [1, 7, 4, -2, 3]
+
+def absolute(x):
+    if x >= 0:
+        return x
+    else:
+        return -x
+
+L2 = sorted(L1, key=absolute)
+print(L2)
+
+#or in reverse order
+print(sorted(L1, reverse=True, key=absolute))
