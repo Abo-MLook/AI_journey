06704356ad397f6ac1a7afbbc43e67f6d5b8eaea
Marwan Fahad <mamemo1270@gmail.com>
2025-12-03 00:57:07 +0300

    a

---
+++ Python_basics/Coursera_path/Data_Collection-Processing/intro_json.py
@@ -0,0 +1,38 @@
+
+"""
+Again, python provides a module for doing this. The module is called json.
+We will be using two functions in this module, loads and dumps.
+
+"""
+# json.loads() takes a string as input and produces a python object (a dictionary or a list) as output.
+import json
+a_string = '\n\n\n{\n "resultCount":25,\n "results": [\n{"wrapperType":"track", "kind":"podcast", "collectionId":10892}]}'
+print(a_string)
+d = json.loads(a_string)
+print("------")
+print(type(d))
+print(d)
+print(d.keys())
+print(d['resultCount'])
+# print(a_string['resultCount'])
+
+
+"""The other function we will use is dumps. It does the inverse of loads. 
+    It takes a python object, typically a dictionary or a list, and returns a string,
+    in JSON format. It has a few other parameters. 
+    Two useful parameters are sort_keys and indent.
+    """
+print()
+print("-" * 20)
+print()
+
+
+def pretty(obj):
+    return json.dumps(obj, sort_keys=True, indent=2)
+
+
+d = {'key1': {'c': True, 'a': 90, '5': 50}, 'key2': {'b': 3, 'c': "yes"}}
+
+print(d)
+print('--------')
+print(pretty(d))
--- Python_basics/Coursera_path/Function_files_Ditcitonary/m5_sorting/ex1.py
+++ Python_basics/Coursera_path/Function_files_Ditcitonary/m5_sorting/ex1.py
@@ -16,3 +16,18 @@ print(L2)
 
 #or in reverse order
 print(sorted(L1, reverse=True, key=absolute))
+winners = ['Alice Munro', 'Alvin E. Roth', 'Kazuo Ishiguro', 'Malala Yousafzai', 'Rainer Weiss', 'Youyou Tu']
+winners.reverse()
+print(winners)
+
+
+medals = {'Japan':41, 'Russia':56, 'South Korea':21, 'United States':121, 'Germany':42, 'China':70}
+top_three = (sorted(medals.keys() , key= lambda k:medals[k]))[:-4:-1]
+print(top_three)
+
+groceries = {'apples': 5, 'pasta': 3, 'carrots': 12, 'orange juice': 2, 'bananas': 8, 'popcorn': 1, 'salsa': 3, 'cereal': 4, 'coffee': 5, 'granola bars': 15, 'onions': 7, 'rice': 1, 'peanut butter': 2, 'spinach': 9}
+
+# Sorting the keys based on their values in descending order
+most_needed = sorted(groceries.keys(), key=lambda k: groceries[k], reverse=True)
+
+print(most_needed)
