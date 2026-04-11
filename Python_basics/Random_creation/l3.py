d5951024f72c7d38d73bccb33cd35a5ef8bfb7ed
Marwan Fahad <mamemo1270@gmail.com>
2026-04-11 13:35:35 +0300

    Update l3.py

--- Python_basics/Random_creation/l3.py
+++ Python_basics/Random_creation/l3.py
@@ -1,2 +1,16 @@
 print(round(3.1231,2))
 print(pow(4,2))
+
+a = 23
+b = a
+print(b is a)
+
+name = "mrwan alayed"
+print(name[1:3])
+g
+print(name.split())
+s = name.split()
+print(" ".join(s))
+print(name.find("d"))
+n = name.replace("m","M")
+print(n)
